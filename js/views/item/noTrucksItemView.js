5d7c7b949f33c86f62bdfdbf9871cfef26b8d52d
Bobby House <bobbyh.house@nuix.com>
2014-09-18 15:38:45 -0400

    Remove code

--- js/views/item/noTrucksItemView.js
+++
@@ -1,8 +0,0 @@
-define(['backbone', 'hbs!tmpl/item/noTrucksItemView-tmpl'],
-
-        function(Backbone, Template) {
-
-            return Backbone.Marionette.ItemView.extend({
-                template: Template
-            });
-});
--- js/views/item/truckItemView.js
+++
@@ -1,8 +0,0 @@
-define(['backbone', 'hbs!tmpl/item/truckItemView-tmpl'],
-
-        function(Backbone, Template) {
-
-            return Backbone.Marionette.ItemView.extend({
-                template: Template
-            });
-});
